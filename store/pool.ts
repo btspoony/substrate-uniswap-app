6375c81391059124947779eb83b83920852e0939
Tang Bo Hao <tech@btang.cn>
2020-09-17 16:51:30 +0800

    store 中添加全部需要实现的接口

--- store/pool.ts
+++ store/pool.ts
@@ -1,22 +1,77 @@
 import { GetterTree, ActionTree, MutationTree } from 'vuex'
 import { RootState } from '~/store'
+import { TradePair } from '~/types'
 
 export const state = () => ({
+  currentIndex: 0,
+  tradePairs: [] as TradePair[]
 })
 
 export type ModuleState = ReturnType<typeof state>
 
 export const getters: GetterTree<ModuleState, RootState> = {
+  currentTradePair: state => state.tradePairs[state.currentIndex]
 }
 
 export const mutations: MutationTree<ModuleState> = {
-  // CHANGE_NAME: (state, newName: string) => (state.name = newName),
+  SETUP_ALL_TRADE_PAIRS: (state, arr: TradePair[]) => (state.tradePairs = arr),
+  SET_CURRENT_TRADE_PAIR: (state, index: number) => {
+    const current = state.tradePairs[index]
+    if (!current) {
+      throw new Error(`failed to set trade pair, index should be [0~${state.tradePairs.length - 1}] instead of ${index}`)
+    }
+    state.currentIndex = index
+  }
 }
 
 export const actions: ActionTree<ModuleState, RootState> = {
-  // async fetchThings({ commit }) {
-  //   const things = await this.$axios.$get('/things')
-  //   console.log(things)
-  //   commit('CHANGE_NAME', 'New name')
-  // },
+  /**
+   * 查询全部交易对
+   */
+  async queryTradePairs (ctx) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    let all: TradePair[] = []
+    // TODO 需要请求 substrate 获取 tokens
+    ctx.commit('SETUP_ALL_TRADE_PAIRS', all)
+  },
+  /**
+   * 创建交易对
+   * 由 管理员 执行
+   */
+  async createNewTradePair (ctx, payload: any) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  },
+  /**
+   * 添加流动性
+   * 由 持币人 执行
+   */
+  async addLiquidityToTradePair (ctx, payload: any) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  },
+  /**
+   * 移出流动性
+   * 由 流动性供应商 执行
+   */
+  async removeLiquidityFromTradePair (ctx, payload: any) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  },
+  /**
+   * 从交易池中以买方操作 Swap
+   * 由 持币人 执行
+   */
+  async buyTokenInTradePair (ctx, payload: any) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  },
+  /**
+   * 从交易池中以卖方操作 Swap
+   * 由 持币人 执行
+   */
+  async sellTokenInTradePair (ctx, payload: any) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  }
 }
--- store/tokens.ts
+++ store/tokens.ts
@@ -1,7 +1,9 @@
 import { GetterTree, ActionTree, MutationTree } from 'vuex'
 import { RootState } from '~/store'
+import { Token } from '~/types'
 
 export const state = () => ({
+  tokens: [] as Token[]
 })
 
 export type ModuleState = ReturnType<typeof state>
@@ -10,13 +12,33 @@ export const getters: GetterTree<ModuleState, RootState> = {
 }
 
 export const mutations: MutationTree<ModuleState> = {
-  // CHANGE_NAME: (state, newName: string) => (state.name = newName),
+  SETUP_ALL_TOKENS: (state, tokens: Token[]) => (state.tokens = tokens)
 }
 
 export const actions: ActionTree<ModuleState, RootState> = {
-  // async fetchThings({ commit }) {
-  //   const things = await this.$axios.$get('/things')
-  //   console.log(things)
-  //   commit('CHANGE_NAME', 'New name')
-  // },
+  /**
+   * 获取全部用户和其私钥
+   */
+  async queryAllTokens (ctx) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    let tokens: Token[] = []
+    // TODO 需要请求 substrate 获取 tokens
+    ctx.commit('SETUP_ALL_TOKENS', tokens)
+  },
+  /**
+   * 创建新代币
+   * 由 管理员 执行
+   */
+  async createNewToken (ctx, payload: { name: string, supply: number }) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  },
+  /**
+   * 代币转账
+   * 由 持币人 执行
+   */
+  async transferToken (ctx, payload: { to: string, amount: number }) {
+    if (!this.$api.isConnected) throw new Error('Node is not connected.')
+    // TODO
+  }
 }
