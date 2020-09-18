3b53bde135e294981099c1b7d495dd9a7f94e3ae
Tang Bo Hao <tech@btang.cn>
2020-09-18 17:31:44 +0800

    tokens 页面实现

--- pages/tokens.vue
+++ pages/tokens.vue
@@ -1,21 +1,34 @@
 <template>
-  <div>
-    Tokens Page
-  </div>
+  <el-tabs
+    v-model="activeTab"
+    :stretch="true"
+  >
+    <el-tab-pane name="tokens" label="Tokens">
+      Content of Tokens
+    </el-tab-pane>
+    <el-tab-pane name="activities" label="Activities">
+      <p>Content of Activities.</p>
+      <p>(Working in progress...)</p>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { Token } from '~/types'
+import { ModuleState } from '~/store/tokens'
 
 @Component({
   async fetch (ctx) {
-    await this.$store.dispatch('tokens/queryAllTokens')
+    await ctx.store.dispatch('tokens/queryAllTokens')
   }
 })
 export default class PageComponent extends Vue {
+  activeTab = "tokens"
   // ---- Computed --
-  // NOTHING
-
+  get availableTokens () {
+    return (this.$store.state.tokens as ModuleState).tokens
+  }
   // ---- Hooks --
   async mounted () {
     // NOTHING
--- pages/trading.vue
+++ pages/trading.vue
@@ -6,16 +6,19 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { TradePair } from '~/types'
+import { ModuleState } from '~/store/pool'
 
 @Component({
   async fetch (ctx) {
-    await this.$store.dispatch('pool/queryTradePairs')
+    await ctx.store.dispatch('pool/queryTradePairs')
   }
 })
 export default class PageComponent extends Vue {
   // ---- Computed --
-  // NOTHING
-
+  get availableTradePairs () {
+    return (this.$store.state.tokens as ModuleState).tradePairs
+  }
   // ---- Hooks --
   async mounted () {
     // NOTHING
